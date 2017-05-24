8b0b1669676133c04adb1bef3762da55dafdecf9
Cristián Rojas <injcristianrojas@gmail.com>
2017-05-24 13:55:43 -0400

    Spring Security integrated. CSRF missing.

---
+++ src/main/java/cl/injcristianrojas/spring/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package cl.injcristianrojas.spring.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+	
+}
---
+++ src/main/java/cl/injcristianrojas/spring/config/WebApplicationInitializer.java
@@ -0,0 +1,22 @@
+package cl.injcristianrojas.spring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { RootConfiguration.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
