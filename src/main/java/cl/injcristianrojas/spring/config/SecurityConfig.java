7b963a9e965d4bca1a23954074f429c07eb73fbd
Cristián Rojas <injcristianrojas@gmail.com>
2017-05-29 14:55:22 -0400

    Changed security config

--- src/main/java/cl/injcristianrojas/spring/config/SecurityConfig.java
+++ src/main/java/cl/injcristianrojas/spring/config/SecurityConfig.java
@@ -5,13 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebMvcSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
 		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
 		auth.inMemoryAuthentication().withUser("jperez").password("123").roles("USER");
 		auth.inMemoryAuthentication().withUser("pdelospalotes").password("abc").roles("USER");
@@ -19,6 +21,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.headers().cacheControl().frameOptions().contentTypeOptions(); // X-XSS-Protection disabled
+		http.headers().cacheControl().frameOptions().contentTypeOptions(); // X-XSS-Protection
 	}
 }
